c2991140a46c0d2e8b86b333817c8d58e1fac3be
ang s <angsala@umich.edu>
2025-11-27 23:31:01 -0500

    add work for pagination and async api fetches

--- radio-poc/src/app/lib/radio-actions.ts
+++ radio-poc/src/app/lib/radio-actions.ts
@@ -3,7 +3,7 @@
 
 const apiDomain = `http://localhost:8080`;
 
-export async function fetchArchive(searchQuery: string, currentPage: number) {
+export async function fetchArchive(currentPage: number) {
   try {
     console.log("query string:");
 
@@ -24,70 +24,6 @@ export async function fetchArchive(searchQuery: string, currentPage: number) {
   }
 }
 
-export async function fetchArchivePageCount(
-  searchQuery: string,
-  currentPage: number
-) {
-  try {
-    const baseUrl = `${apiDomain}/archive/numpages`;
-    const params = new URLSearchParams({
-      query: searchQuery,
-      page: currentPage.toString(),
-    });
-    const fullUrl = `${baseUrl}?${params.toString()}`;
-    const res = await fetch(fullUrl, {
-      method: "GET",
-    });
-    const numcount = await res.json();
-    return numcount["numPages"];
-  } catch (err) {
-    console.error("Error in fetching archive count:", err);
-    throw new Error("Failed to fetch archive count.");
-  }
-}
-
-export async function fetchShowInfo(showslug: string) {
-  try {
-    const baseUrl = `${apiDomain}/shows`;
-    const params = new URLSearchParams({
-      query: showslug,
-    });
-    const fullUrl = `${baseUrl}?${params.toString()}`;
-    const res = await fetch(fullUrl, {
-      method: "GET",
-    });
-
-    const showData = await res.json();
-    return showData;
-  } catch (err) {
-    console.error("Error in fetching show", err);
-    throw new Error("failed in fetching show information.");
-  }
-}
-
-export async function fetchShowArchive(
-  searchQuery: string,
-  currentPage: number,
-  showslug: string
-) {
-  try {
-    const baseUrl = `${apiDomain}/show-archive/` + showslug;
-    const params = new URLSearchParams({
-      query: searchQuery,
-      page: currentPage.toString(),
-    });
-    const fullUrl = `${baseUrl}?${params.toString()}`;
-    const res = await fetch(fullUrl, {
-      method: "GET",
-    });
-
-    const showData = await res.json();
-    return showData;
-  } catch (err) {
-    console.error("Error in fetching mixes from this show", err);
-    throw new Error("failed in fetching show mixes.");
-  }
-}
 
 export async function fetchSchedule(){
 
@@ -104,6 +40,21 @@ export async function fetchSchedule(){
 
     console.error("Error in fetching schedule", err);
     throw new Error("Error in fetching schedule.");
+  }
+}
+
+export async function fetchInitialGetLiveStream(){
+  try {
+    const fullUrl = `https://api.radiocult.fm/api/station/redux-731b6892/schedule/live`;
+    const res = await fetch(fullUrl, {
+      method: "GET",
+    });
+
+    const retrievedLiveData = await res.json();
+    return retrievedLiveData;
+  } catch (err) {
+    console.error("Error in fetching initial get live stream", err);
+    throw new Error("Error in fetching initial get live stream .");
   }
 }
 
