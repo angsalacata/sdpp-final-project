24c51faf0cc5fe95c0d76dcd2a83ce40feb6df9f
ang s <angsala@umich.edu>
2025-11-27 23:32:16 -0500

    fixed persistent player

--- radio-poc/src/app/players/persistent-player.tsx
+++ radio-poc/src/app/players/persistent-player.tsx
@@ -1,19 +1,42 @@
 'use client'
-
-
 import { useContext } from "react"
 import { RadioAudioContext, useRadioAudioContext } from "../contexts/AudioContext"
 import { PlaybackButtons } from "../ui/playback-buttons"
-import "@/app/styles/persistent-player.scss"
+import "../styles/globals.scss"
+import clsx from "clsx";
+import React from "react"
+
+
 
 export function PersistentPlayer(){
 
-    const {audioHTML, setCurrentTime, setDuration, currentMixName, imageUrl} = useRadioAudioContext();
+    const {audioHTML, setCurrentTime, setDuration, currentMixName, imageUrl, currentShowName, currentdjName} = useRadioAudioContext();
+    const isPlaying = currentMixName !== "";
 
+    console.log("what is playing")
+    console.log(isPlaying)
     return(
-        <div className="persistent-player">
-            <audio ref={audioHTML}/>
-            <PlaybackButtons></PlaybackButtons>
+        
+        <div
+            className={clsx("persistent-player-container", {
+                isPlaying: isPlaying,
+            })}
+            >
+            <div className="persistent-player">
+                <div className="playbutton-wrap">
+                <audio ref={audioHTML}/>
+                <div className="playbutton-wrap">
+                <PlaybackButtons></PlaybackButtons> 
+                </div>
+                <div className="player-mix-info">
+                {currentMixName && (
+                <React.Fragment>
+                    <div className="show">{currentShowName}</div>
+                    <div className="host">{currentdjName}</div>
+                </React.Fragment> )}
+                </div>
+                </div>
+            </div>
             
         </div>
     );
